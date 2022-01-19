ce64bb0c3b81e01a42e7478a137913f6627498ea
l <lvenxu@163.com>
2022-01-19 17:25:35 +0800

    版本1

--- src/main/java/com/example/demo2/controller/IndexController.java
+++ src/main/java/com/example/demo2/controller/IndexController.java
@@ -1,8 +1,13 @@
 package com.example.demo2.controller;
 
+import com.example.demo2.bean.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.thymeleaf.util.StringUtils;
+
+import javax.servlet.http.HttpSession;
 
 @Controller
 public class IndexController {
@@ -14,13 +19,32 @@ public class IndexController {
     }
 
     @PostMapping("/login")//防止表单重复提交
-    public String main(String username,String password){
+    public String main(User user, HttpSession session,Model model){
+        if(!StringUtils.isEmpty(user.getUserName()) && "123456".equals(user.getPassword()))
+        {
+            session.setAttribute("loginUser",user);
+            return "redirect:/main.html";
+        }else
+        {
+            model.addAttribute("msg","账号密码错误");
+            return  "login";
+        }
 
-        return "redirect:/main.html";
+
     }
 
     @GetMapping("/main.html")
-    public String mainPage(){
-        return "main";
+    public String mainPage(HttpSession session,Model model){
+
+        //拦截器 过滤器
+        Object loginUser = session.getAttribute("loginUser");
+        if(loginUser != null)
+        {
+            return "main";
+        }else{
+            model.addAttribute("msg","请重新登陆");
+            return "login";
+        }
+
     }
 }
---
+++ src/main/java/com/example/demo2/controller/TableController.java
@@ -0,0 +1,41 @@
+package com.example.demo2.controller;
+
+import com.example.demo2.bean.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Controller
+public class TableController {
+
+    @GetMapping("/basic_table")
+    public String basic_table(){
+        return "table/basic_table";
+    }
+
+    @GetMapping("/dynamic_table")
+    public String dynamic_table(Model model){
+        //表格内容的遍历
+        List<User> users = Arrays.asList(new User("zhangsan", "123456"),
+                new User("lisi", "123444"),
+                new User("ha", "aaaaaa"),
+                new User("hehe", "aaaddd")
+        );
+        model.addAttribute("users",users);
+        return "table/dynamic_table";
+    }
+
+    @GetMapping("/responsive")
+    public String responsive_table(){
+        return "table/responsive_table";
+    }
+
+    @GetMapping("/editable_table")
+    public String editable_table(){
+        return "table/editable_table";
+    }
+
+}
